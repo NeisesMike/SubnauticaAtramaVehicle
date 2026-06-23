13dc06580374d4cea3ec54280cc37fbd4d7491ca
Michael Neises <neisesmichael@gmail.com>
2026-06-23 13:28:08 -0500

    Report forcible unlocks conservatively
    Don't over-report

--- VehicleFramework/VehicleFramework/MiscComponents/VFUnlocker.cs
+++ VehicleFramework/VehicleFramework/MiscComponents/VFUnlocker.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
 using System.Text;
@@ -17,33 +18,45 @@ namespace VehicleFramework.MiscComponents
         }
         private IEnumerator UnlockEverythingMomentarily()
         {
-            bool somethingHappened = false;
+            List<string> unlockedTechs = new();
             yield return new WaitForSeconds(5f);
 
             //unlock vehicles that should be unlocked
             var list = Admin.VehicleManager.vehicleTypes
+                .Where(v => !KnownTech.Contains(v.mv.TechType))
                 .Where(v => v.mv.UnlockedWith == TechType.None || KnownTech.Contains(v.mv.UnlockedWith))
                 .ToList();
             list.ForEach(v => KnownTech.Add(v.techType, true));
 
             //unlock upgrades that should be unlocked
-            foreach(var uppy in Admin.UpgradeRegistrar.RegisteredUpgrades)
+            foreach (var uppy in Admin.UpgradeRegistrar.RegisteredUpgrades)
             {
-                if(uppy.Key.UnlockAtStart || KnownTech.Contains(uppy.Key.UnlockWith))
+                if (KnownTech.Contains(uppy.Key.TechTypes.forSeamoth)
+                    || KnownTech.Contains(uppy.Key.TechTypes.forExosuit)
+                    || KnownTech.Contains(uppy.Key.TechTypes.forCyclops)
+                    || KnownTech.Contains(uppy.Key.TechTypes.forModVehicle))
+                {
+                    // don't trigger on already-known technologies
+                    continue;
+                }
+                if (uppy.Key.UnlockAtStart || KnownTech.Contains(uppy.Key.UnlockWith))
                 {
                     // These should be TechType.None if nothing was registered, but adding None to KnownTech is harmless, so we can just add them all.
                     KnownTech.Add(uppy.Value.forSeamoth, true);
                     KnownTech.Add(uppy.Value.forExosuit, true);
                     KnownTech.Add(uppy.Value.forCyclops, true);
                     KnownTech.Add(uppy.Value.forModVehicle, true);
-                    somethingHappened = true;
+                    unlockedTechs.Add(uppy.Key.TechTypes.forModVehicle.AsString());
                 }
             }
 
-            if(list.Count > 0 || somethingHappened)
+            // report whether we had to forcibly unlock something, and what it was
+            if (list.Count > 0 || unlockedTechs.Count > 0)
             {
                 Logger.PDANote("Some Vehicle Framework technologies were unlocked.");
             }
+            list.ForEach(x => Logger.Log($"Unlocked vehicle: {x.techType.AsString()}"));
+            unlockedTechs.ForEach(x => Logger.Log($"Unlocked upgrade: {x}"));
 
             Component.DestroyImmediate(this);
         }
